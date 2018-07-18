dbc3d3e2c768960b156f461b89c8cdba8f804e80
nira <nayzaa2010@hotmail.com>
2018-07-18 14:31:41 +0700

    delete tag

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -69,8 +69,6 @@
               </div>
             </div>
 
-          </div>
-
           <div class="form-group row mb-0">
             <div class="col-md-8 offset-md-4">
               <button type="submit" class="uk-button uk-button-primary">
