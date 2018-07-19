ed8e2b62e1a54483df2e038250b623358805ff3b
nira <nayzaa2010@hotmail.com>
2018-07-19 14:21:08 +0700

    update

--- database/seeds/StaffSeeder.php
+++ database/seeds/StaffSeeder.php
@@ -11,12 +11,23 @@ class StaffSeeder extends Seeder
   */
   public function run()
   {
-    DB::table('users')->insert([
-      'email'=>'staff@gmail.com',
-      'password'=>Hash::make('7744536'),
-      'firstname'=>'staff',
-      'lastname'=>'staff',
-      'is_staff'=>1,
-    ]);
+    DB::table('users')->insert(
+      [
+        [
+          'email'=>'staff@gmail.com',
+          'password'=>Hash::make('7744536'),
+          'firstname'=>'staff',
+          'lastname'=>'staff',
+          'is_staff'=>1,
+        ],
+        [
+          'email'=>'staff2@gmail.com',
+          'password'=>Hash::make('111111'),
+          'firstname'=>'staff2',
+          'lastname'=>'staff2',
+          'is_staff'=>1,
+        ]
+      ]
+    );
   }
 }
