ba2dad63609a3d9e4a71d3a6ea234c5e20210cff
nira <nayzaa2010@hotmail.com>
2018-07-23 02:55:13 +0700

    payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -24,162 +24,158 @@ use App\Product;
 
 class PaymentController extends Controller
 {
-    public function getCheckoutSummary()
-    {
-      $cart = [];
-      $total_price =0;
-      if(session()->has('cart')){
-        $session_cart = Session::get('cart');
-        foreach ($session_cart as $index => $value) {
-          $id =$session_cart[$index]['id'];
-          $cart[$index] = Product::find($id);
-          $cart[$index]['amount'] = $session_cart[$index]['amount'];
-          $cart[$index]['product_price'] = $cart[$index]['product_price']*$cart[$index]['amount'];
-          $total_price += $cart[$index]['product_price'];
-        }
+  private $_api_context;
+
+  public function getCheckoutSummary()
+  {
+    $cart = [];
+    $total_price =0;
+    if(session()->has('cart')){
+      $session_cart = Session::get('cart');
+      foreach ($session_cart as $index => $value) {
+        $id =$session_cart[$index]['id'];
+        $cart[$index] = Product::find($id);
+        $cart[$index]['amount'] = $session_cart[$index]['amount'];
+        $cart[$index]['product_price'] = $cart[$index]['product_price']*$cart[$index]['amount'];
+        $total_price += $cart[$index]['product_price'];
+      }
+    }
+    return view('order.order-checkout',['cart'=>$cart,'total_price'=>$total_price]);
+  }
+
+  public function nextProcess()
+  {
+    Session::push('next-process','next');
+    return redirect()->back();
+  }
+
+  /**
+  * Create a new controller instance.
+  *
+  * @return void
+  */
+  public function __construct()
+  {
+
+    /** PayPal api context **/
+    $paypal_conf = \Config::get('paypal');
+    $this->_api_context = new ApiContext(new OAuthTokenCredential(
+      $paypal_conf['client_id'],
+      $paypal_conf['secret'])
+    );
+    $this->_api_context->setConfig($paypal_conf['settings']);
+
+  }
+
+  public function payWithpaypal(Request $request)
+  {
+
+    $payer = new Payer();
+    $payer->setPaymentMethod('paypal');
+
+    $item_1 = new Item();
+
+    $item_1->setName('Item 1') /** item name **/
+    ->setCurrency('THB')
+    ->setQuantity(1)
+    ->setPrice($request->get('amount')); /** unit price **/
+
+    $item_list = new ItemList();
+    $item_list->setItems(array($item_1));
+
+    $amount = new Amount();
+    $amount->setCurrency('THB')
+    ->setTotal($request->get('amount'));
+
+    $transaction = new Transaction();
+    $transaction->setAmount($amount)
+    ->setItemList($item_list)
+    ->setDescription('Bobby CARTless Shop');
+
+    $redirect_urls = new RedirectUrls();
+    $redirect_urls->setReturnUrl(URL::to('status')) /** Specify return URL **/
+    ->setCancelUrl(URL::to('status'));
+
+    $payment = new Payment();
+    $payment->setIntent('Sale')
+    ->setPayer($payer)
+    ->setRedirectUrls($redirect_urls)
+    ->setTransactions(array($transaction));
+    /** dd($payment->create($this->_api_context));exit; **/
+    try {
+      $payment->create($this->_api_context);
+
+    } catch (\PayPal\Exception\PPConnectionException $ex) {
+
+      if (\Config::get('app.debug')) {
+
+        \Session::put('error', 'Connection timeout');
+        return Redirect::to('/');
+
+      } else {
+
+        \Session::put('error', 'Some error occur, sorry for inconvenient');
+        return Redirect::to('/');
+
       }
 
+    }
 
-      return view('order.order-checkout',['cart'=>$cart,'total_price'=>$total_price]);
+    foreach ($payment->getLinks() as $link) {
+
+      if ($link->getRel() == 'approval_url') {
+
+        $redirect_url = $link->getHref();
+        break;
+
+      }
 
     }
 
-    private $_api_context;
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+    /** add payment ID to session **/
+    Session::put('paypal_payment_id', $payment->getId());
 
-        /** PayPal api context **/
-        $paypal_conf = \Config::get('paypal');
-        $this->_api_context = new ApiContext(new OAuthTokenCredential(
-            $paypal_conf['client_id'],
-            $paypal_conf['secret'])
-        );
-        $this->_api_context->setConfig($paypal_conf['settings']);
+    if (isset($redirect_url)) {
 
-    }
-    public function index()
-    {
-        return view('paywithpaypal');
-    }
-    public function payWithpaypal(Request $request)
-    {
-
-        $payer = new Payer();
-        $payer->setPaymentMethod('paypal');
-
-        $item_1 = new Item();
-
-        $item_1->setName('Item 1') /** item name **/
-            ->setCurrency('USD')
-            ->setQuantity(1)
-            ->setPrice($request->get('amount')); /** unit price **/
-
-        $item_list = new ItemList();
-        $item_list->setItems(array($item_1));
-
-        $amount = new Amount();
-        $amount->setCurrency('USD')
-            ->setTotal($request->get('amount'));
-
-        $transaction = new Transaction();
-        $transaction->setAmount($amount)
-            ->setItemList($item_list)
-            ->setDescription('Your transaction description');
-
-        $redirect_urls = new RedirectUrls();
-        $redirect_urls->setReturnUrl(URL::to('status')) /** Specify return URL **/
-            ->setCancelUrl(URL::to('status'));
-
-        $payment = new Payment();
-        $payment->setIntent('Sale')
-            ->setPayer($payer)
-            ->setRedirectUrls($redirect_urls)
-            ->setTransactions(array($transaction));
-        /** dd($payment->create($this->_api_context));exit; **/
-        try {
-            $payment->create($this->_api_context);
-
-        } catch (\PayPal\Exception\PPConnectionException $ex) {
-
-            if (\Config::get('app.debug')) {
-
-                \Session::put('error', 'Connection timeout');
-                return Redirect::to('/');
-
-            } else {
-
-                \Session::put('error', 'Some error occur, sorry for inconvenient');
-                return Redirect::to('/');
-
-            }
-
-        }
-
-        foreach ($payment->getLinks() as $link) {
-
-            if ($link->getRel() == 'approval_url') {
-
-                $redirect_url = $link->getHref();
-                break;
-
-            }
-
-        }
-
-        /** add payment ID to session **/
-        Session::put('paypal_payment_id', $payment->getId());
-
-        if (isset($redirect_url)) {
-
-            /** redirect to paypal **/
-            return Redirect::away($redirect_url);
-
-        }
-
-        \Session::put('error', 'Unknown error occurred');
-        return Redirect::to('/');
+      /** redirect to paypal **/
+      return Redirect::away($redirect_url);
 
     }
 
-    public function getPaymentStatus()
-    {
-        /** Get the payment ID before session clear **/
-        $payment_id = Session::get('paypal_payment_id');
+    \Session::put('error', 'Unknown error occurred');
+    return Redirect::to('/');
 
-        /** clear the session payment ID **/
-        Session::forget('paypal_payment_id');
-        if (empty(Input::get('PayerID')) || empty(Input::get('token'))) {
+  }
 
-            \Session::put('error', 'Payment failed');
-            return Redirect::to('/');
+  public function getPaymentStatus()
+  {
+    /** Get the payment ID before session clear **/
+    $payment_id = Session::get('paypal_payment_id');
 
-        }
+    /** clear the session payment ID **/
+    Session::forget('paypal_payment_id');
+    if (empty(Input::get('PayerID')) || empty(Input::get('token'))) {
 
-        $payment = Payment::get($payment_id, $this->_api_context);
-        $execution = new PaymentExecution();
-        $execution->setPayerId(Input::get('PayerID'));
+      \Session::put('error', 'Payment failed');
+      return Redirect::to('/');
 
-        /**Execute the payment **/
-        $result = $payment->execute($execution, $this->_api_context);
-
-        if ($result->getState() == 'approved') {
-
-            \Session::put('success', 'Payment success');
-            return Redirect::to('/');
-
-        }
-
-        \Session::put('error', 'Payment failed');
-        return Redirect::to('/');
-
-    public function nextProcess()
-    {
-      Session::push('next-process','next');
-      return redirect()->back();
     }
+
+    $payment = Payment::get($payment_id, $this->_api_context);
+    $execution = new PaymentExecution();
+    $execution->setPayerId(Input::get('PayerID'));
+
+    /**Execute the payment **/
+    $result = $payment->execute($execution, $this->_api_context);
+
+    if ($result->getState() == 'approved') {
+
+      \Session::put('success', 'Payment success');
+      return Redirect::to('/');
+
+    }
+
+    \Session::put('error', 'Payment failed');
+    return Redirect::to('/');
+  }
 }
